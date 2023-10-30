f95d6d8bfae23b6a56689da5befdcee580df5c48
Thiago Willecke <thiagogwillecke@gmail.com>
2023-10-30 16:30:01 -0300

    Integrate sign up form with API

--- client/src/RegistersForm.tsx
+++ client/src/RegistersForm.tsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { ChangeEvent, useState } from "react";
 
 export default function RegisterForm() {
@@ -9,16 +10,33 @@ export default function RegisterForm() {
 		password: "",
 	});
 
-	function SignUpClickHandler() {
-		const loginInputData = {
+	const SignUpClickHandler = (e: React.FormEvent) => {
+		e.preventDefault(); // Prevent the default form submission
+		const registrationData = {
 			name: formData.name,
-			lastName: formData.lastName,
-			emailAddress: formData.emailAddress,
+			lastname: formData.lastName,
+			email: formData.emailAddress,
 			username: formData.username,
 			password: formData.password,
 		};
-		console.log(loginInputData);
-	}
+
+		axios
+			.post(
+				"http://localhost:3000/user",
+				registrationData,
+			)
+			.then((response) => {
+				if (response.status === 201) {
+					console.log("Registration successful");
+					// You can add further actions like redirecting to a success page here
+				} else {
+					console.log("Registration failed");
+				}
+			})
+			.catch((error) => {
+				console.error("Error:", error);
+			});
+	};
 
 	const handleInputChange = (
 		e: ChangeEvent<HTMLInputElement>,
@@ -32,29 +50,29 @@ export default function RegisterForm() {
 		<div className="flex justify-center items-center h-screen">
 			<div className="bg-neutral-600 p-10 rounded-md">
 				<h2 className="mb-4">Register Form</h2>
-				<form className="flex flex-col">
+				<form onSubmit={SignUpClickHandler}>
 					<div>
-					<input
-						name="name-input"
-						className="mb-3 mr-3 p-3 rounded-md"
-						type="text"
-						placeholder="Name"
-						value={formData.name}
-						onChange={(e) => handleInputChange(e, "name")}
-					></input>
-					<input
-						name="last-name-input"
-						className="mb-3 p-3 rounded-md"
-						type="text"
-						placeholder="Last Name"
-						value={formData.lastName}
-						onChange={(e) => handleInputChange(e, "lastName")}
-					></input>
+						<input
+							name="name-input"
+							className="mb-3 mr-3 p-3 rounded-md"
+							type="text"
+							placeholder="Name"
+							value={formData.name}
+							onChange={(e) => handleInputChange(e, "name")}
+						></input>
+						<input
+							name="last-name-input"
+							className="mb-3 p-3 rounded-md"
+							type="text"
+							placeholder="Last Name"
+							value={formData.lastName}
+							onChange={(e) => handleInputChange(e, "lastName")}
+						></input>
 					</div>
 					<input
 						name="email-input"
 						className="mb-12 p-3 rounded-md"
-						type="text"
+						type="email" // Use type="email" for email validation
 						placeholder="E-mail Address"
 						value={formData.emailAddress}
 						onChange={(e) => handleInputChange(e, "emailAddress")}
@@ -62,7 +80,7 @@ export default function RegisterForm() {
 					<h2 className="mb-4">Login Data</h2>
 					<input
 						name="username-input"
-						className="mb-3 p-3 rounded-md"
+						className="mb-3 mr-3 p-3 rounded-md"
 						type="text"
 						placeholder="Username"
 						value={formData.username}
@@ -78,15 +96,14 @@ export default function RegisterForm() {
 					></input>
 					<div className="flex justify-end">
 						<a
-							href="login"
+							href="/login" // Link to the login page
 							className="pt-2 text-zinc-300 hover:text-zinc-400"
 						>
 							Login
 						</a>
 						<button
-							onClick={SignUpClickHandler}
 							className="pt-2 text-blue-400 hover:text-blue-500 focus:border-transparent hover:border-transparent"
-							type="button"
+							type="submit" // Change type to "submit"
 						>
 							Sign Up
 						</button>
