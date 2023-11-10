8441e62ce6b3d7036ea7bb4790e7b53ca9e3b887
Thiago Willecke <thiagogwillecke@gmail.com>
2023-11-10 11:54:35 -0300

    add further data validation to user credentials creation

--- server/src/domain/entity/UserCredentials.ts
+++ server/src/domain/entity/UserCredentials.ts
@@ -1,15 +1,19 @@
 export default class UserCredentials {
-	constructor(
-		readonly name: string,
-		readonly lastname: string,
-		readonly email: string,
+    constructor(
+        readonly name: string,
+        readonly lastname: string,
+        readonly email: string,
         readonly username: string,
         readonly password: string,
-	) {
-        if (name === "") throw new Error("Name is required");
-        if (lastname === "") throw new Error("Last name is required");
-        if (email === "") throw new Error("Email is required");
-        if (username === "") throw new Error("Username is required");
-        if (password === "") throw new Error("Password is required");
+    ) {
+        if (!name) throw new Error("Name cannot be null or undefined");
+        if (!lastname) throw new Error("Last name cannot be null or undefined");
+        if (!email || !this.isValidEmail(email)) throw new Error("Invalid email format");
+        if (!username) throw new Error("Username cannot be null or undefined");
+        if (!password) throw new Error("Password cannot be null or undefined");
+    }
+
+    private isValidEmail(email: string): boolean {
+        return /\S+@\S+\.\S+/.test(email);
     }
 }
